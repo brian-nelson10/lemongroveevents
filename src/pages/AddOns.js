f0427f6d8784f63604fd42416331f03a7dd2a863
brian nelson <bn3l10@gmail.com>
2023-03-08 23:40:50 -0500

    responsive test

--- src/pages/AddOns.js
+++ src/pages/AddOns.js
@@ -83,8 +83,8 @@ const AddOns= () => {
             initial="initial"
             animate="animate"
             exit="exit"
-            className="mx-[1rem] md:mx-[2rem] mt-[7rem] z-50">
-        <section className="grid grid-cols-2 -mx-3 md:mx-[2rem] flex items-center h-[30%] w-vw mb-10 bg-fixed bg-center bg-cover rounded-sm drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] addon-head">
+            className="mx-[1rem] lg:mx-[2rem] mt-[7rem] z-50">
+        <section className="grid grid-cols-2 -mx-3 lg:mx-[2rem] flex items-center h-[30%] w-vw mb-10 bg-fixed bg-center bg-cover rounded-sm drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] addon-head">
   <motion.div 
     variants={pack}
     initial="initial"
@@ -93,45 +93,45 @@ const AddOns= () => {
   Add On's 
   </motion.div>
 </section>
-<section className="text-center font-roboto font-bold tracking-wide text-[#283845] mt-6 md:-mb-14">
+<section className="text-center font-roboto font-bold tracking-wide text-[#283845] mt-6 lg:-mb-14">
 
-<div className="text-[2.2rem] md:text-[3rem] font-larissa">Customize your package.</div>
+<div className="text-[2.2rem] lg:text-[3rem] font-larissa">Customize your package.</div>
 
-<div className="text-xl md:text-[1.5rem] mb-[1rem] md:mb-1 md:-mt-2">If you have a special vision, we will deliver your special day.</div>
-<hr className="mb-[1rem] md:mt-6"/>
+<div className="text-xl lg:text-[1.5rem] mb-[1rem] lg:mb-1 lg:-mt-2">If you have a special vision, we will deliver your special day.</div>
+<hr className="mb-[1rem] lg:mt-6"/>
 </section>
-<section className="overflow-hidden text-bold tracking-wide md:text-xl text-[#283845] h-[35rem] md:h-auto z-50">
-  <div className="container md:mx-auto md:px-5 py-2 lg:px-32 lg:pt-24">
-    <div className="-m-1 flex flex-wrap md:-m-2">
+<section className="overflow-hidden text-bold tracking-wide lg:text-xl text-[#283845] h-[35rem] lg:h-auto z-50">
+  <div className="container lg:mx-auto lg:px-5 py-2 lg:px-32 lg:pt-24">
+    <div className="-m-1 flex flex-wrap lg:-m-2">
       <div className="flex w-1/2 flex-wrap">
-        <div className="-mb-[6rem] p-1 md:p-2">
+        <div className="-mb-[6rem] p-1 lg:p-2">
           <Image
             srcset={grazerWeb}
             fallback={grazer}
             alt="grazer table"
             className="block h-full w-full rounded-lg object-cover object-center"/>
-          <div className="mt-6 mb-[4rem] md:mb-1 font-larissa font-bold text-lg md:text-xl">Charcuterie Grazing Table.</div>
+          <div className="mt-6 mb-[4rem] lg:mb-1 font-larissa font-bold text-lg lg:text-xl">Charcuterie Grazing Table.</div>
         </div>
-        <div className="w-full p-1 md:p-2">
+        <div className="w-full p-1 lg:p-2">
           <Image
             srcset={champagneWeb}
             fallback={champagne}
             alt="bubbles of champagne"
             className="block h-full w-full rounded-lg object-cover object-center"/>
-            <div className="mt-6 font-larissa font-bold text-lg md:text-xl ">Champagne.</div>
+            <div className="mt-6 font-larissa font-bold text-lg lg:text-xl ">Champagne.</div>
         </div>
       </div>
       <div className="flex w-1/2 flex-wrap">
-        <div className="w-full p-1 md:p-2">
+        <div className="w-full p-1 lg:p-2">
         <Image
             srcset={floralWeb}
             fallback={floral}
             alt="floral arrangement"
             className="block h-full w-full rounded-lg object-cover object-center"/>
-            <div className="mt-6 font-larissa font-bold text-lg md:text-xl text-end">Floral Arrangements.</div>
+            <div className="mt-6 font-larissa font-bold text-lg lg:text-xl text-end">Floral Arrangements.</div>
         </div>
-        <div className="-mb-[6rem] p-1 md:p-2">
-        <div className="mb-6 font-larissa font-bold text-lg md:text-xl">Custom Hotel Parties.</div>
+        <div className="-mb-[6rem] p-1 lg:p-2">
+        <div className="mb-6 font-larissa font-bold text-lg lg:text-xl">Custom Hotel Parties.</div>
           <Image
             srcset={hotelWeb}
             fallback={hotel}
@@ -143,9 +143,9 @@ const AddOns= () => {
     </div>
   </div>
 </section>
-{/* <section className="grid mt-[22rem] -ml-[3rem] md:mb-[9rem] z-40"> */}
+{/* <section className="grid mt-[22rem] -ml-[3rem] lg:mb-[9rem] z-40"> */}
     <LazyLoadComponent>
-    <div className="grid mt-[42rem] -mb-[60rem] md:-mb-[45rem] z-40" >
+    <div className="grid mt-[42rem] -mb-[60rem] lg:-mb-[45rem] z-40" >
     <Lemon />
     </div>
     </LazyLoadComponent>
--- src/pages/PackageOne.js
+++ src/pages/PackageOne.js
@@ -91,9 +91,9 @@ const PackageOne= () => {
             initial="initial"
             animate="animate"
             exit="exit"
-            className="mx-[1rem] md:mx-[2rem] mt-[7rem] -mb-[16rem] z-40">
+            className="mx-[1rem] lg:mx-[2rem] mt-[7rem] -mb-[16rem] z-40">
         <section 
-          className="grid grid-cols-2 -mx-3 md:mx-[2rem] flex items-center h-[30%] w-vw mb-10 bg-fixed rounded-sm drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] date-head"
+          className="grid grid-cols-2 -mx-3 lg:mx-[2rem] flex items-center h-[30%] w-vw mb-10 bg-fixed rounded-sm drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] date-head"
           >
   <motion.div 
     variants={pack}
@@ -109,14 +109,14 @@ const PackageOne= () => {
   <div className="grid lg:grid-cols-2 flex items-center">
     <div className="mb-12 lg:mb-0">
       <div
-        className="block rounded-lg shadow-[0_35px_80px_5px_rgba(0,0,1,1)] px-6 py-12 lg:py-6 xl:py-12 md:px-12 lg:-mr-14"
+        className="block rounded-lg shadow-[0_35px_80px_5px_rgba(0,0,1,1)] px-6 py-12 lg:py-6 xl:py-12 lg:px-12 lg:-mr-14"
         style={{ background: "hsla(0, 0%, 100%, 0.55)", backdropFilter: "blur(30px)"}}
       >
-      <h3 className="text-lg md:text-[2rem] text-[#283845] font-bold font-roboto mb-6">Do you want to plan a perfect date to show your partner how much they mean to you?</h3>
+      <h3 className="text-lg lg:text-[2rem] text-[#283845] font-bold font-roboto mb-6">Do you want to plan a perfect date to show your partner how much they mean to you?</h3>
       <h5 className="text-xl text-[#283845] font-roboto font-bold tracking-wide ml-4 lg:mb-4 xl:mb-6">Let us deliver your perfect day.</h5>
 <div className="grid grid-cols-3">
     <div className="col-span-3">
-      <p className="font-bold text-end font-larissa text-[3rem] md:text-[4rem] text-[#283845] tracking-widest md:-mt-[2rem] -mb-4 justify-start">Date.</p>
+      <p className="font-bold text-end font-larissa text-[3rem] lg:text-[4rem] text-[#283845] tracking-widest lg:-mt-[2rem] -mb-4 justify-start">Date.</p>
       </div>
       </div>
       <ul className="mx-4">
@@ -143,7 +143,7 @@ const PackageOne= () => {
         </li>
         
       </ul>
-      <div className="grid md:grid-cols-4 gap-4 z-60">
+      <div className="grid lg:grid-cols-4 gap-4 z-60">
          <div className="grid z-60 text-center "> 
              <Button children="Book Now" onClick={handleBook} />
          </div>
@@ -151,7 +151,7 @@ const PackageOne= () => {
       </div> 
     </div>
     {width < breakpoint ?
-    <div className="absolute mr-4 mb-[4rem] -bottom-[30rem]">
+    <div className="absolute mr-4 mb-[4rem] -bottom-[40rem]">
     <Image 
         srcset={mobileWeb} 
         fallback={mobile}
@@ -166,7 +166,7 @@ const PackageOne= () => {
   </div>
 </div>
 </section>
-<section className="grid -mt-[15rem] -ml-[3rem] mb-[32rem] md:mb-[9rem] z-10">
+<section className="grid -mt-[15rem] -ml-[3rem] mb-[32rem] lg:mb-[9rem] z-10">
     <br/>
     <LazyLoadComponent>
     <div className="grid z-50" >
@@ -180,13 +180,13 @@ const PackageOne= () => {
       <svg className="rotate-[90deg] hover:cursor-pointer" width="36" height="66" viewBox="0 0 26 46" fill="#283845" stroke="#283845" strokeWidth="0" opacity=".9" xmlns="http://www.w3.org/2000/svg">
 <path d="M7.2929 45.7071C7.68342 46.0976 8.31659 46.0976 8.70711 45.7071L15.0711 39.3431C15.4616 38.9526 15.4616 38.3195 15.0711 37.9289C14.6805 37.5384 14.0474 37.5384 13.6569 37.9289L8 43.5858L2.34315 37.9289C1.95263 37.5384 1.31946 37.5384 0.928936 37.9289C0.538412 38.3195 0.538412 38.9526 0.928936 39.3431L7.2929 45.7071ZM7 1.02965e-07L7 45L9 45L9 -1.02965e-07L7 1.02965e-07Z"/>
 </svg>
-<p className="font-larissa font-bold tracking-wide text-md -mt-7 -ml-3 md:opacity-0 group-hover:opacity-100">Home</p>
+<p className="font-larissa font-bold tracking-wide text-md -mt-7 -ml-3 lg:opacity-0 group-hover:opacity-100">Home</p>
       </div>
       <div className="grid col-span-1 flex flex-row mr-[2rem] -mt-[14px] group justify-end" onClick={handleEvent}>
       <svg className="-rotate-[90deg] hover:cursor-pointer" width="36" height="66" viewBox="0 0 26 46" fill="#283845" stroke="#283845" strokeWidth="0" opacity=".9" xmlns="http://www.w3.org/2000/svg">
 <path d="M7.2929 45.7071C7.68342 46.0976 8.31659 46.0976 8.70711 45.7071L15.0711 39.3431C15.4616 38.9526 15.4616 38.3195 15.0711 37.9289C14.6805 37.5384 14.0474 37.5384 13.6569 37.9289L8 43.5858L2.34315 37.9289C1.95263 37.5384 1.31946 37.5384 0.928936 37.9289C0.538412 38.3195 0.538412 38.9526 0.928936 39.3431L7.2929 45.7071ZM7 1.02965e-07L7 45L9 45L9 -1.02965e-07L7 1.02965e-07Z"/>
 </svg>
-<div className="font-larissa font-bold tracking-wide text-md md:opacity-0 group-hover:opacity-100">Event</div>
+<div className="font-larissa font-bold tracking-wide text-md lg:opacity-0 group-hover:opacity-100">Event</div>
       </div>
     </div>
         </motion.main>
